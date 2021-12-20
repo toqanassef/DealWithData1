405885b3fdd1f094cb62e88a68fb9127e115daac
Toqa Nassef <toqanassef@gmail.com>
2021-12-20 15:39:27 +0200

    HolidayApi

--- Enozom1/Controllers/HolidayController.cs
+++ Enozom1/Controllers/HolidayController.cs
@@ -27,5 +27,78 @@ namespace Enozom1.Controllers
                 .ToList();
             return Holidays;
         }
+        [HttpPost]
+        public IActionResult Add(int CountryId ,int HolidayId )
+        {
+            try
+            {
+                var IfExist = _context.countryHolidays.Where
+                    (a => a.CountryId == CountryId && a.HolidayId == HolidayId)
+                    .Any();
+                if (IfExist)
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    var countryHoliday = new CountryHolidays
+                    {
+                        CountryId = CountryId,
+                        HolidayId = HolidayId
+                    };
+                    _context.countryHolidays.Add(countryHoliday);
+                    _context.SaveChanges();
+                    return Ok();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            
+        }
+        [HttpDelete]
+        public IActionResult Delete (int CountryId, int HolidayId)
+        {
+            try
+            {
+                var DeleteHoliday = _context.countryHolidays.Where
+                    (a => a.CountryId == CountryId && a.HolidayId == HolidayId)
+                    .FirstOrDefault();
+                if (DeleteHoliday == null)
+                    return NotFound();
+    
+                _context.countryHolidays.Remove(DeleteHoliday);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        [HttpPut]
+        public IActionResult Update(int HolidayId , string HolidayName)
+        {
+            try
+            {
+                var OldHoliday = _context.Holiday.Where
+                    (a => a.Id == HolidayId)
+                    .FirstOrDefault();
+                if (OldHoliday == null)
+                    return NotFound();
+
+                OldHoliday.Name = HolidayName;
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+        
     }
 }
